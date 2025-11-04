9647a301bde9a0784b11eed31eebab056b846de5
htayyi <hhtayyi@gmail.com>
2025-11-04 17:06:02 +0530

    message

---
+++ user.py
@@ -0,0 +1,5 @@
+# Prompt the user to enter their name and store the input in a variable
+user_name = input("Please enter your name: ")
+
+# Display a greeting message that includes the entered name
+print(f"Hello, {user_name}! Welcome.")
